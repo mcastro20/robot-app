8c65f6ef4f8d1f02b99ea29d569e07437660423e
michael castro <m.castro@multisoft.com>
2019-06-27 10:20:11 +0800

    Load component in index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from 'react-dom'
+import Table from './components/Table'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <Table robotPosition={[4, 0]} />,
+    document.getElementById('root'),
+)
