97f2a54c30e5a03f441d0eca228148b6159b3c0c
michael castro <m.castro@multisoft.com>
2019-06-27 10:19:24 +0800

    Added Robot Component

---
+++ src/components/Robot.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Robot() {
+  return (
+    <span
+      role="img"
+      aria-label="robot"
+      style={{
+        position: "relative",
+        fontSize: "15vh",
+      }}
+    >
+      🤖
+    </span>
+  );
+}
